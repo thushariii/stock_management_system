ed7f5c5ae78a900fe00744a1dad118f06520abbc
thushariii <thusharipahalage@gmail.com>
2021-11-08 20:52:02 +0530

    initial_ commit

---
+++ stock/index.php
@@ -0,0 +1,150 @@
+<?php 
+require_once 'php_action/db_connect.php';
+
+session_start();
+
+if(isset($_SESSION['userId'])) {
+	header('location: http://localhost/stock/dashboard.php');	
+}
+
+$errors = array();
+
+if($_POST) {		
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(empty($username) || empty($password)) {
+		if($username == "") {
+			$errors[] = "Username is required";
+		} 
+
+		if($password == "") {
+			$errors[] = "Password is required";
+		}
+	} else {
+		$sql = "SELECT * FROM users WHERE username = '$username'";
+		$result = $connect->query($sql);
+
+		if($result->num_rows == 1) {
+			$password = md5($password);
+			// exists
+			$mainSql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+			$mainResult = $connect->query($mainSql);
+
+			if($mainResult->num_rows == 1) {
+				$value = $mainResult->fetch_assoc();
+				$user_id = $value['user_id'];
+
+				// set session
+				$_SESSION['userId'] = $user_id;
+
+				header('location: http://localhost/stock/dashboard.php');	
+			} else{
+				
+				$errors[] = "Incorrect username/password combination";
+			} // /else
+		} else {		
+			$errors[] = "Username doesnot exist";		
+		} // /else
+	} // /else not empty username // password
+	
+} // /if $_POST
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stock Management System</title>
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+	<!-- bootstrap theme-->
+	<link rel="stylesheet" href="assests/bootstrap/css/bootstrap-theme.min.css">
+	<!-- font awesome -->
+	<link rel="stylesheet" href="assests/font-awesome/css/font-awesome.min.css">
+
+  <!-- custom css -->
+  <link rel="stylesheet" href="custom/css/custom.css">	
+
+  <!-- jquery -->
+	<script src="assests/jquery/jquery.min.js"></script>
+  <!-- jquery ui -->  
+  <link rel="stylesheet" href="assests/jquery-ui/jquery-ui.min.css">
+  <script src="assests/jquery-ui/jquery-ui.min.js"></script>
+
+  <!-- bootstrap js -->
+</head>
+
+
+<body style="background-image: url(img/a.jpg)">
+
+	<div class="container">
+		<div class="row vertical">
+			<div class="col-md-5 col-md-offset-4">
+				<div class="panel panel-info" style="background: #f9f9f9;
+	                                    padding: 10px;
+	                                    margin-top: 20px;
+	                                    border-top-right-radius: 30px;
+	                                    border-top-left-radius: 30px; border-bottom-right-radius: 30px;
+	                                    border-bottom-left-radius: 30px;" 
+                                        span="margin: 3px;
+	                                    font-size: 20px;
+	                                    font-weight: bold;">
+					<div class="panel-heading" style="background:  #CBC3E3">
+						<h3 class="panel-title" style="text-align: center; text: bold; color: black"> Inventory Login </h3>
+					</div>
+					<div class="panel-body">
+
+						<div class="messages">
+							<?php if($errors) {
+								foreach ($errors as $key => $value) {
+									echo '<div class="alert alert-warning" role="alert">
+									<i class="glyphicon glyphicon-exclamation-sign"></i>
+									'.$value.'</div>';										
+									}
+								} ?>
+						</div>
+
+						<form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" id="loginForm">
+							<fieldset>
+							  <div class="form-group">
+									<label for="username" class="col-sm-2 control-label">Username</label>
+									<div class="col-sm-10">
+									  <input type="text" class="form-control" id="username" name="username" placeholder="Username" autocomplete="off" />
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password" class="col-sm-2 control-label">Password</label>
+									<div class="col-sm-10">
+									  <input type="password" class="form-control" id="password" name="password" placeholder="Password" autocomplete="off" />
+									</div>
+								</div>								
+								<div class="form-group">
+									<div class="col-sm-offset-2 col-sm-10">
+									  <button type="submit" class="btn btn-default" style="margin:auto; display:block;"> <i class="glyphicon glyphicon-log-in"></i> Login</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<!-- panel-body -->
+				</div>
+				<!-- /panel -->
+			</div>
+			<!-- /col-md-4 -->
+		</div>
+		<!-- /row -->
+	</div>
+
+	<!-- container -->	
+</body>
+</html>
+
+
+
+
+
+
+
+	
